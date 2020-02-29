4ac133752c26377883c37b7ad30393d188bd7f1b
Joe <carre.joe3@gmail.com>
2020-02-29 16:09:26 +0000

    setup mongodb and db connection

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,16 @@
+const mongoDB = require('mongodb')
+
 module.exports = (app) => {
-    app.post('/register', (req, res) => {
-        res.send(`Hello ${req.body.username}, you are registered!`)
-    })
+  app.post('/register', async (req, res) => {
+    const users = await loadUsersCollection()
+    res.send(await users.find({}).toArray())
+  })
+}
+
+async function loadUsersCollection () {
+  const client = await mongoDB.MongoClient.connect('mongodb+srv://dbUser:3d3nC0urt@personaldashboard-irjzv.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+  })
+
+  return client.db('PersonalDashboard').collection('users')
 }
