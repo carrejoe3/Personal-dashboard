aa322db93f28a6aaa432e4585d036bea29b44f2e
Joe <carre.joe3@gmail.com>
2020-02-29 15:26:12 +0000

    sequelize and sqlite3 setup

---
+++ server/src/models/user.js
@@ -0,0 +1,13 @@
+module.export = (sequelize, DataTypes) => {
+  sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: DataTypes.STRING
+  })
+}
