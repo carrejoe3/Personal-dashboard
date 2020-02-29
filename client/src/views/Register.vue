897c71df179ba4f53be7c92259e13870837b0cab
Joe <carre.joe3@gmail.com>
2020-02-29 16:39:40 +0000

    Updated front end when user is registered
    -form elements are disabled
    -register button disappears and 'Back to login' button appears
    -a snackbar displays

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -11,19 +11,23 @@
               </v-toolbar>
               <v-card-text>
                 <v-form>
-                  <v-text-field label="Username" name="username" type="text" v-model="username"/>
-                  <v-text-field label="Email" name="email" type="text" v-model="email"/>
-                  <v-text-field id="password" label="Password" name="password" type="password" v-model="password"/>
-                  <v-text-field id="confirmPassword" label="Confirm password" name="confirmPassword" type="password" v-model="confirmPassword"/>
+                  <v-text-field label="Username" name="username" type="text" v-model="username" :disabled="userRegistered"/>
+                  <v-text-field label="Email" name="email" type="text" v-model="email" :disabled="userRegistered"/>
+                  <v-text-field id="password" label="Password" name="password" type="password" v-model="password" :disabled="userRegistered"/>
+                  <v-text-field id="confirmPassword" label="Confirm password" name="confirmPassword" type="password" v-model="confirmPassword" :disabled="userRegistered"/>
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer />
-                <v-btn color="primary" @click="registerUser">Register</v-btn>
+                <v-btn color="primary" @click="registerUser" v-if="!userRegistered">Register</v-btn>
+                <v-btn color="primary" @click="registerUser" v-if="userRegistered" to="/">Back to login</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
         </v-row>
+        <v-snackbar v-model="snackbar">Registration complete!
+          <v-btn color="red" text @click="snackbar = false">Close</v-btn>
+        </v-snackbar>
       </v-container>
     </v-content>
   </v-app>
@@ -38,16 +42,22 @@ export default {
     username: '',
     email: '',
     password: '',
-    confirmPassword: ''
+    confirmPassword: '',
+    userRegistered: false,
+    snackbar: false
   }),
   methods: {
     async registerUser () {
-      const response = authenication.register({
+      const response = await authenication.register({
         username: this.username,
         email: this.email,
         password: this.password
       })
-      console.log(response)
+
+      if (response.status === 201) {
+        this.userRegistered = true
+        this.snackbar = true
+      }
     }
   }
 }
