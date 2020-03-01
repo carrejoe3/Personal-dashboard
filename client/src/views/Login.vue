87b3a8482626841d0b61ecbe1b06fd53bd0b272f
Joe <carre.joe3@gmail.com>
2020-03-01 19:53:39 +0000

    styled the login page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,25 +1,30 @@
 <template>
-  <v-app id="inspire">
+  <v-app>
     <v-content>
-      <v-container class="fill-height" fluid>
+      <v-container fluid>
         <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="4">
-            <v-card class="elevation-12">
-              <v-toolbar color="primary" dark flat>
-                <v-toolbar-title>Login form</v-toolbar-title>
-                <v-spacer />
+          <v-col cols="12" sm="10" md="10">
+            <v-card id="loginContainer" flat>
+              <v-toolbar id="toolbar" flat>
+                <v-spacer></v-spacer>
+                <v-toolbar-title id="title">Hackathon</v-toolbar-title>
+                <v-spacer></v-spacer>
               </v-toolbar>
               <v-card-text>
-                <v-form>
-                  <v-text-field label="Username" name="username" type="text" v-model="username"/>
-                  <v-text-field id="password" label="Password" name="password" type="password" v-model="password"/>
+                <v-form id="loginInputs">
+                  <v-text-field dark label="Username" name="username" type="text" v-model="username"/>
+                  <v-spacer></v-spacer>
+                  <v-text-field dark id="password" label="Password" name="password" type="password" v-model="password"/>
                 </v-form>
               </v-card-text>
               <v-card-actions>
-                <v-spacer />
-                <v-btn color="primary" @click="login">Login</v-btn>
-                <v-btn to="/register" color="primary">Register</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn id="loginBtn" @click="login" depressed>
+                  <v-img src="@/public/Login_button.png" width="200px"></v-img>
+                </v-btn>
+                <v-spacer></v-spacer>
               </v-card-actions>
+              <div id="bottomText">New to the hackothon? <span id="signUpText" @click="goToRegistrationPage">Sign up</span></div>
             </v-card>
           </v-col>
         </v-row>
@@ -72,7 +77,52 @@ export default {
       } catch (error) {
         console.error(error)
       }
+    },
+    goToRegistrationPage () {
+      this.$router.push('/register')
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+#title {
+  font-size: 3em;
+  color: white;
+}
+
+#loginContainer {
+  background: none;
+
+  #toolbar {
+    background: none;
+  }
+}
+
+#loginInputs {
+  margin-top: 10%;
+  display: flex;
+}
+
+#loginBtn {
+  margin-top: 10%;
+  background: none;
+}
+
+#bottomText {
+  margin-top: 10%;
+  text-align: center;
+  font-size: 20px;
+  color: white;
+
+  #signUpText {
+    color: yellow;
+  }
+}
+</style>
+
+<style lang="scss">
+#app {
+  background: url('~@/public/Background.png')
+}
+</style>
