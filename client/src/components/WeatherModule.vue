7e548c242d167f767da0aa35e97b08a82c6a9da3
Joe <carre.joe3@gmail.com>
2020-03-01 12:33:44 +0000

    set weather icon from fetched weather data

--- client/src/components/WeatherModule.vue
+++ client/src/components/WeatherModule.vue
@@ -6,7 +6,9 @@
           <v-list-item-title class="headline mb-1">{{ this.weatherData.name }}</v-list-item-title>
           <v-list-item-subtitle>{{ this.weatherData.main.temp | toCelsius}}</v-list-item-subtitle>
         </v-list-item-content>
-        <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+        <v-list-item-avatar tile size="80">
+          <v-img :src="weatherIcon"></v-img>
+        </v-list-item-avatar>
       </v-list-item>
     </v-card>
   </v-container>
@@ -19,6 +21,7 @@ import axios from 'axios'
 export default {
   data: () => ({
     apiKey: 'd0a10211ea3d36b0a6423a104782130e',
+    weatherIcon: 'https://openweathermap.org/img/wn/10d@2x.png',
     weatherData: {
       name: 'Loading...',
       main: {
@@ -34,10 +37,14 @@ export default {
         )
         .then(response => {
           this.weatherData = response.data
+          this.setWeatherIcon()
         })
         .catch(error => {
           console.error(error)
         })
+    },
+    setWeatherIcon () {
+      this.weatherIcon = 'http://openweathermap.org/img/wn/' + this.weatherData.weather[0].icon + '@2x.png'
     }
   },
   filters: {
