214972cf9ded2f9f22e4640301caa373f88ad435
Joe <carre.joe3@gmail.com>
2020-03-01 21:50:20 +0000

    added config on backend for production

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const config = require('./config/config')
+const path = require('path')
 
 const app = express()
 
@@ -14,5 +15,16 @@ app.use(cors())
 const routes = require('./routes')
 
 app.use('', routes)
+
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(path.join(__dirname, '/public/')))
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+  })
+}
 
 app.listen(config.port, () => console.log(`Server started on port ${config.port}`))
