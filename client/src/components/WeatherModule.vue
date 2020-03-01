d6465f9c45629a3002f331a19f718a20096996c3
Joe <carre.joe3@gmail.com>
2020-03-01 12:12:20 +0000

    display the location name and temperature in celsius

--- client/src/components/WeatherModule.vue
+++ client/src/components/WeatherModule.vue
@@ -1,6 +1,13 @@
 <template>
   <v-container class="grey lighten-5">
     <v-card class="mx-auto" max-width="344">
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1">{{ this.weatherData.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ this.weatherData.main.temp | toCelsius}}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+      </v-list-item>
     </v-card>
   </v-container>
 </template>
@@ -12,17 +19,30 @@ import axios from 'axios'
 export default {
   data: () => ({
     apiKey: 'd0a10211ea3d36b0a6423a104782130e',
-    weatherData: null
+    weatherData: {
+      name: 'Loading...',
+      main: {
+        temp: 'Loading...'
+      }
+    }
   }),
   methods: {
     fetchWeatherData (position) {
-      axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${this.apiKey}`)
-        .then((response) => {
+      axios
+        .get(
+          `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${this.apiKey}`
+        )
+        .then(response => {
           this.weatherData = response.data
         })
-        .catch((error) => {
+        .catch(error => {
           console.error(error)
         })
+    }
+  },
+  filters: {
+    toCelsius: function (value) {
+      return Math.round(value - 273.15) + ' degrees'
     }
   },
   mounted () {
