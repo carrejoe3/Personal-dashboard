7cd47ecdf5ef407adba772875a4846354e0ab398
Joe <carre.joe3@gmail.com>
2020-03-01 21:17:29 +0000

    styled the news article component

--- client/src/views/NewsArticle.vue
+++ client/src/views/NewsArticle.vue
@@ -1,11 +1,10 @@
 <template>
   <v-container>
-    <v-card class="mx-auto" max-width="344">
-      <v-img src="https://ichef.bbci.co.uk/news/660/cpsprodpb/10C8D/production/_111094786_hi060304309.jpg" height="200px"></v-img>
-      <v-card-title>{{ this.newsArticle.rss.channel.item[0].title._cdata }}</v-card-title>
-      <v-divider class="mx-4"></v-divider>
+    <v-card class="mx-auto" flat>
+      <v-img src="https://ichef.bbci.co.uk/news/660/cpsprodpb/10C8D/production/_111094786_hi060304309.jpg" height="400px"></v-img>
+      <v-card-title class="justify-center" id="title">{{ this.newsArticle.rss.channel.item[0].title._cdata }}</v-card-title>
       <v-card-text>
-        <div class="my-4 subtitle-1">{{ this.newsArticle.rss.channel.item[0].description._cdata }}</div>
+        <div id="content" class="my-4 subtitle-1">{{ this.newsArticle.rss.channel.item[0].description._cdata }}</div>
       </v-card-text>
     </v-card>
     <HomeButton />
@@ -27,3 +26,23 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+#app {
+  .theme--light.v-card {
+    background-color: transparent;
+    border: none;
+
+    .v-card__title {
+      background: transparent;
+      color: white;
+      font-size: 2.2em;
+      margin-top: 10%;
+    }
+  }
+}
+
+#content {
+  color: white;
+}
+</style>
