e7eaf291e65302504ad38da6cc485a55e164f533
Joe <carre.joe3@gmail.com>
2020-03-01 21:41:28 +0000

    added a dev proxy

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,15 @@
+const path = require('path')
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '': {
+        target: 'http://localhost:8081'
+      }
+    }
+  },
+  outputDir: path.resolve(__dirname, '../server/public')
 }
